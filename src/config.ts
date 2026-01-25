2929cb57ec2d5ed26758beb359bbc8248f38c18a
lkb <tmwlak@gmail.com>
2026-01-25 13:20:18 +0900

    정상작동

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,9 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-  title: "blog",
-  subtitle: "subtitle",
+  title: "leekb.com",
+  // subtitle: "subtitle",
+  subtitle: "",
   themeColor: {
     hue: 275, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false, // Hide the theme color picker for visitors
@@ -34,7 +35,8 @@ export const siteConfig: SiteConfig = {
   },
   favicon: [
     {
-      src: "/favicon/icon.webp",
+      // src: "/favicon/icon.webp",
+      src: "/favicon.ico",
       //   theme: 'light',
       //   sizes: '32x32',
     },
